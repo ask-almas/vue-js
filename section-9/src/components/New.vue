b07a006faa83f6be771c055b48f0bde50e9c401b
ask-almas <ask.almas@gmail.com>
2019-01-04 18:41:16 +0600

    test

---
+++ section-9/src/components/New.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h3>The New</h3>
+        <button @click="counter++">Increase</button>
+        <p>{{counter}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                counter: 0
+            };
+        },
+        destroyed() {
+            console.log('Desctroyed');
+        },
+        activated() {
+            console.log('Activated');
+        },
+        deactivated() {
+            console.log('Deactivated');
+        }
+    }
+</script>
+
+<style>
+</style>
