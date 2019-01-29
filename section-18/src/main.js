0b193739a60aa5144b6eca2af85414ccbdfdfaa6
ask-almas <ask.almas@gmail.com>
2019-01-29 19:11:38 +0600

    test

---
+++ section-18/src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import {routes} from "./routes";
+import store from './store/store'
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-stock-trader-b9a84.firebaseio.com/';
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
+
+Vue.filter('currency', (value)=>{
+  return '$' + value.toLocaleString();
+});
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+});
